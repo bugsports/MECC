7d05cf899aa36598c9aa28894f7cc5d6da4e440c
bugsports <serial_experiment@outlook.com>
2022-05-06 10:42:22 -0400

    update with multithread

--- PortScanner.py
+++ PortScanner.py
@@ -1,26 +1,61 @@
-# Port Scanner
-import socket
-import sys
-from datetime import datetime
+# Port Scanner with Multi Threading, comments for study
+import socket #https://docs.python.org/3/library/socket.html
+import threading #https://docs.python.org/3/library/threading.html
+from queue import Queue #https://docs.python.org/3/library/queue.html
 
-ipv4 = input("Enter an IP address to scan: ")
-print("Scanning ports on: " + str(ipv4))
-print("-"*50)
+# thread number
+THREAD_N = 200
+print_lock = threading.Lock()
 
-try:
-    for port in range(1,65535): #max port number
+def main():
+    host = input("Enter the IP address to scan: ")
+    ports = range(1,1025) # range of sockets (up to 65535 + 1)
+
+    def port_scanner(port):
+        # creates socket (IPv4, TCP connection)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try: 
+            # establishes connection
+            con = sock.connect((host, port))
+            # prevents printing the same port
+            with print_lock:
+                print("Port",port,"status: open")
+            # closes connection when done
+            con.close()
+        except:
+            pass
 
-        result = sock.connect_ex((ipv4,port))
-        if result == 0:
-            print("Port {}:  Open".format(port))
-        sock.close()
-except KeyboardInterrupt:
-    print("\n Exiting scanner...")
-    sys.exit()
-except socket.gaierror:
-    print("\n Hostname couldn't be resolved.")
-    sys.exit()
-except socket.error:
-    print("\n Server not responding.")
-    sys.exit()
+    def threader():
+        while True:
+            # receives worker from the queue
+            worker = q.get()
+            # calls port_scanner function, using the number of workers from
+            # the queue as the port number (see line 44).
+            port_scanner(worker)
+            # after scanner is complete, queue is emptied
+            q.task_done()
+            
+    # stores "tasks"
+    q = Queue()
+            
+    # creates threads
+    for t in range(THREAD_N):
+        # creating a thread using threader() function as target
+        t = threading.Thread(target=threader)
+        # causes thread to die with the main thread completes
+        t.daemon = True
+        # starts thread
+        t.start()
+    
+    # assigns number of jobs to queue according to number of ports
+    for worker in ports:
+        q.put(worker)
+    
+    q.join()
+    
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nProgram exiting...")
+        quit()
